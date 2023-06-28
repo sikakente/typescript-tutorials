a48fe1aa4b3c9048cd42a5b9904ed8e0a016cdd5
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2023-06-28 13:47:12 +0100

    Setup docker compose for db connection

--- todo-api/index.ts
+++ todo-api/index.ts
@@ -4,10 +4,21 @@ import express, {
   Response,
 } from 'express';
 import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 
 // Instantiate the express app
 const app: Express = express();
 dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true, // Tells TypeORM to delete databases: Do not use in Production
+});
 
 // Define server port
 const port = process.env.PORT;
@@ -17,5 +28,14 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript server');
 });
 
-// Start listening
-app.listen(port);
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port);
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error(
+      'Error during Data Source initialization',
+      err,
+    );
+  });
